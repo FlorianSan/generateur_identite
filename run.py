7811927d22f1ce04194cf699d41c0c3974f252ce
flo12 <fl.sansou@gmail.com>
2019-03-26 14:06:09 +0100

    File creation

--- run.py
+++ run.py
@@ -1,9 +1,3 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return "Hello World!"
-
+from app.views import app
 if __name__ == "__main__":
     app.run(host="localhost", port=8080)
